53092981df2fc8917d7bfbd2f5f92e6cc4fd3bbf
Joker-qp <mhmd.duyar@gmail.com>
2026-08-10 17:05:15 +0300

    hata düzeltmeleri

--- start.bat
+++ start.bat
@@ -5,16 +5,19 @@ echo.
 echo Leafai Web App Starting...
 echo.
 
-if not exist .venv (
-    echo Creating virtual environment (.venv)...
-    python -m venv .venv
-    call .venv\Scripts\activate.bat
-    python -m pip install --upgrade pip
-    pip install -r requirements.txt
-) else (
-    call .venv\Scripts\activate.bat
-)
+if exist .venv goto HASVENV
 
+echo Creating virtual environment...
+python -m venv .venv
+call .venv\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+goto RUNAPP
+
+:HASVENV
+call .venv\Scripts\activate.bat
+
+:RUNAPP
 echo Starting server at http://127.0.0.1:8000
 start http://127.0.0.1:8000
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
